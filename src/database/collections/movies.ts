420176919441a2ed19c920306b187116c997b75d
nilsishome <nilsnyberg24@gmail.com>
2025-05-12 02:52:39 +0200

    Add environment variables for database user + password

--- src/database/collections/movies.ts
+++ src/database/collections/movies.ts
@@ -2,8 +2,10 @@ import mongoose from "mongoose";
 import { Movies } from "../models";
 
 export async function retrieveMovies() {
-  const uri =
-    "mongodb+srv://nilsnyberg24:yIGZm59YQC0CaqvH@kinocluster.dtam1oe.mongodb.net/Kino?retryWrites=true&w=majority&appName=KinoCluster";
+  const USERNAME = process.env.MONGO_USER;
+  const PASSWORD = process.env.MONGO_PASS;
+
+  const uri = `mongodb+srv://${USERNAME}:${PASSWORD}@kinocluster.dtam1oe.mongodb.net/Kino?retryWrites=true&w=majority&appName=KinoCluster`;
   await mongoose.connect(uri);
   const allMovies = await Movies.find();
 
