16a8a711e7b3e9df01edb8ad20cba333e57cf593
nilsishome <nilsnyberg24@gmail.com>
2025-05-14 22:38:31 +0200

    More styling for mobile view

--- components/movies/[movieId]/text.tsx
+++ components/movies/[movieId]/text.tsx
@@ -9,14 +9,20 @@ type Props = {
 const Text: React.FC<Props> = ({ movie }) => {
   return (
     <>
-      <Typography variant="h1" sx={{ marginBottom: "1rem" }}>
+      <Typography
+        variant="h1"
+        sx={{
+          margin: { xs: "5% 0 2% 5%", sm: "0 0 1% 5%" },
+          fontSize: { xs: "2rem", sm: "3rem" },
+        }}
+      >
         {movie.title}{" "}
         <Typography
           variant="body1"
           sx={{
             display: "inline",
             fontWeight: "600",
-            fontSize: "1.4rem",
+            fontSize: { xs: "1.2rem", sm: "1.4rem" },
           }}
         >
           ({movie.release})
@@ -24,13 +30,22 @@ const Text: React.FC<Props> = ({ movie }) => {
       </Typography>
       <Typography
         variant="body1"
-        sx={{ marginBottom: "0.5%", fontSize: "0.95rem", color: "#adadad" }}
+        sx={{
+          margin: "0 0 0.5% 5%",
+          fontSize: "0.95rem",
+          color: "#adadad",
+        }}
       >
         {movie.genre}
       </Typography>
       <Typography
         variant="body1"
-        sx={{ fontWeight: "600", width: "50%", paddingBottom: "1.5%" }}
+        sx={{
+          fontWeight: "600",
+          width: { xs: "90%", sm: "70%", md: "50%" },
+          paddingBottom: "1.5%",
+          marginLeft: "5%",
+        }}
       >
         {movie.story}
       </Typography>
