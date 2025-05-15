a2d0750d2a7564626987c8b17f861f9563f9296a
Tobias Larsson <tobbe-larsson@hotmail.com>
2025-05-15 11:59:03 +0200

    Added dynamic loop for swiper component

--- components/index/carousel.tsx
+++ components/index/carousel.tsx
@@ -7,40 +7,38 @@ import { Navigation } from "swiper/modules";
 import SwiperButtons from "./SwiperButtons";
 import Image from "next/image";
 
-
-interface Movie { 
-  _id: string; 
-  title: string; 
+interface Movie {
+  _id: string;
+  title: string;
   sliderImage: string;
   genre: string;
-  hour: number; 
+  hour: number;
   minute: number;
 }
 
-const CarouselComponent: React.FC = () => { 
+const CarouselComponent: React.FC = () => {
   const [movies, setMovies] = useState<Movie[]>([]);
 
-  const useFeaturedOnly = true; // True only shows movies from original design. False show all in API.
+  const useFeaturedOnly = false; // True only shows movies from original design. False show all in API.
 
-  useEffect(() => { 
-    async function fetchMovies() { 
-      try { 
-        const res = await fetch ("/api/movies"); 
-        const json = await res.json(); 
+  useEffect(() => {
+    async function fetchMovies() {
+      try {
+        const res = await fetch("/api/movies");
+        const json = await res.json();
 
-      if (useFeaturedOnly) { 
-        const featuredTitles = [ 
-          "Psycho",
-          "Studie i brott",
-          "Singin' In The Rain",
-        ];
-        const filtered = json.data.filter((movie: Movie) => 
-        featuredTitles.includes(movie.title)
-      );
-      setMovies(filtered);
-      } else 
-        setMovies(json.data);
-      } catch (err) { 
+        if (useFeaturedOnly) {
+          const featuredTitles = [
+            "Psycho",
+            "Studie i brott",
+            "Singin' In The Rain",
+          ];
+          const filtered = json.data.filter((movie: Movie) =>
+            featuredTitles.includes(movie.title)
+          );
+          setMovies(filtered);
+        } else setMovies(json.data);
+      } catch (err) {
         console.error("Failed to fetch movies", err);
       }
     }
@@ -63,7 +61,7 @@ const CarouselComponent: React.FC = () => {
         style={{ width: "100vw", maxWidth: "1280px" }}
         spaceBetween={0}
         slidesPerView={1}
-        loop
+        loop={movies.length > 2}
         navigation={{
           nextEl: ".swiper-button-next",
           prevEl: ".swiper-button-prev",
@@ -94,27 +92,28 @@ const CarouselComponent: React.FC = () => {
                 }}
               />
               <Box
-              sx={{
-                position: "absolute",
-                bottom: 0,
-                left: 0,
-                width: "100%",
-                background: "rgba(0,0,0,0.5)",
-                color: "white",
-                padding: "16px",
-                boxSizing: "border-box",
-              }}
-              > 
-              <h2 style={{ margin: 0, fontSize: "1.5rem", color: "#f1ddc5"}}>{movie.title}</h2>
-              <p style={{ margin: 0, color: "#f1ddc5"}}>
-                {movie.genre} | {movie.hour}h {movie.minute}min
-              </p>
+                sx={{
+                  position: "absolute",
+                  bottom: 0,
+                  left: 0,
+                  width: "100%",
+                  background: "rgba(0,0,0,0.5)",
+                  color: "white",
+                  padding: "16px",
+                  boxSizing: "border-box",
+                }}
+              >
+                <h2 style={{ margin: 0, fontSize: "1.5rem", color: "#f1ddc5" }}>
+                  {movie.title}
+                </h2>
+                <p style={{ margin: 0, color: "#f1ddc5" }}>
+                  {movie.genre} | {movie.hour}h {movie.minute}min
+                </p>
               </Box>
             </Box>
-
           </SwiperSlide>
         ))}
- <SwiperButtons />
+        <SwiperButtons />
       </Swiper>
     </Box>
   );
