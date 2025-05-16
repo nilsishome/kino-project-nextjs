e8fff6de2e0dc201cbe4554431553ec631233572
nilsishome <nilsnyberg24@gmail.com>
2025-05-16 12:21:26 +0200

    Revert "Render single movies hero + styling"

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  images: {
-    remotePatterns: [
-      { protocol: "https", hostname: "m.media-amazon.com" },
-      { protocol: "https", hostname: "raw.githubusercontent.com" },
-    ],
-  },
 };
 
 export default nextConfig;
