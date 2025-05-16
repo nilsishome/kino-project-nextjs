cda494f21a5a1157e14cf9c01730df4a96e03d87
nilsishome <nilsnyberg24@gmail.com>
2025-05-16 12:31:40 +0200

    Undo merge with style-moviespage

--- src/app/movies/page.tsx
+++ src/app/movies/page.tsx
@@ -1,7 +1,5 @@
 import Link from "next/link";
 import { Movie } from "@/types";
-import { Box, Typography } from "@mui/material";
-import * as styles from "./moviesPage.style";
 
 export default async function Page() {
   const response = await fetch("http://localhost:3000/api/movies");
@@ -14,22 +12,18 @@ export default async function Page() {
   const movies: Movie[] = payload.data;
 
   return (
-    <Box sx={styles.container}>
-      <Typography sx={styles.title}>Movies</Typography>
-      <Box sx={styles.list}>
+    <>
+      <h1>Movies</h1>
+      <ul>
         {movies.map((movie) => (
-          <Box key={movie._id} sx={styles.listItem}>
+          <li key={movie._id}>
             <Link href={`/movies/${movie._id}`}>
-              <img
-                src={movie.coverImage}
-                alt={movie.title}
-                style={styles.image}
-              />
-              <Typography sx={styles.titleText}>{movie.title}</Typography>
+              <img src={movie.coverImage} alt={movie.title} />
+              <p>{movie.title}</p>
             </Link>
-          </Box>
+          </li>
         ))}
-      </Box>
-    </Box>
+      </ul>
+    </>
   );
 }
