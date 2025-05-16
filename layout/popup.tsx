e3bef778c90cb9eea7f7665b9bd59d53a995404a
Magnus <49763392+MagnusTerak@users.noreply.github.com>
2025-05-16 11:15:25 +0200

    Created structure for booking popup & fixed Mui Outlined styling in theme to match our design

---
+++ layout/popup.tsx
@@ -0,0 +1,77 @@
+"use client";
+
+import React from "react";
+
+import { Box, Fade, Typography, Stepper, StepLabel, Step } from "@mui/material";
+
+const steps = ["Biljettbokning", "Platsbokning", "Inloggning"];
+
+export default function popup() {
+  const [activeStep, setActiveStep] = React.useState(0);
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleReset = () => {
+    setActiveStep(0);
+  };
+
+  return (
+    <Fade in={true}>
+      <Box
+        sx={{
+          backgroundColor: "#1A323C",
+          border: "2px solid white",
+          width: "80vw",
+          height: "80vh",
+          margin: "auto",
+          borderRadius: "3px",
+        }}
+      >
+        <Box
+          sx={{
+            backgroundColor: "#374B54",
+            width: "95%",
+            height: "7vh",
+            borderRadius: "2rem",
+            margin: "2vh auto 0 auto",
+          }}
+        >
+          <Typography
+            sx={{
+              fontFamily: "yesteryear",
+              fontSize: "3rem",
+              color: "#F2753B",
+              paddingLeft: "3rem",
+              paddingTop: "0.5%",
+              height: "100%",
+            }}
+          >
+            Retro
+          </Typography>
+        </Box>
+        <Stepper
+          activeStep={activeStep}
+          orientation="horizontal"
+          sx={{
+            width: "60vw",
+            margin: "2rem auto auto auto",
+          }}
+        >
+          {steps.map((step, index) => (
+            <Step key={step}>
+              <StepLabel>{step}</StepLabel>
+            </Step>
+          ))}
+        </Stepper>
+
+        {/* Booking components under here */}
+      </Box>
+    </Fade>
+  );
+}
