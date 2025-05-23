23704f3a8cff3cd3a3b8bb811d7570572620ac87
Tobias Larsson <tobbe-larsson@hotmail.com>
2025-05-23 11:46:39 +0200

    Refactored buttons for genre, decade and filmtype. Created new file FilterButtons and import button logic from that to filteringsection file to make the latter shorter.

--- components/index/FilteringSection.tsx
+++ components/index/FilteringSection.tsx
@@ -10,6 +10,7 @@ import {
 } from "@mui/material";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
+import FilterButtons from "../index/FilterButtons";
 
 const localTheme = createTheme({
   components: {
@@ -31,44 +32,44 @@ const localTheme = createTheme({
 
 const genres: string[] = ["Thriller", "Komedi", "Äventyr", "Action", "Drama"];
 const decades: { label: string; value: string }[] = [
-  {label: "50-tal", value: "50"},
- { label: "60-tal", value: "60"},
- { label: "70-tal", value: "70"},
-  {label: "80-tal", value: "80"},
-  {label: "90-tal", value: "90"},
- {label:  "00-tal", value: "00"},
-  {label: "10-tal", value: "10"},
+  { label: "50-tal", value: "50" },
+  { label: "60-tal", value: "60" },
+  { label: "70-tal", value: "70" },
+  { label: "80-tal", value: "80" },
+  { label: "90-tal", value: "90" },
+  { label: "00-tal", value: "00" },
+  { label: "10-tal", value: "10" },
 ];
 const filmTypes: string[] = ["Svartvit", "Färgfilm"];
 
 //Helper function for rendering Select-components for mobile view.
-const mobileSelectSX = {color: "#f1ddc5", border: "2px solid #f1ddc5"};
+const mobileSelectSX = { color: "#f1ddc5", border: "2px solid #f1ddc5" };
 
-function renderSelect( 
+function renderSelect(
   value: string,
   setValue: (value: string) => void,
   placeholder: string,
-  options: (string | {label: string; value: string })[]
-) { 
-  return ( 
+  options: (string | { label: string; value: string })[]
+) {
+  return (
     <Select
-    value={value}
-    onChange={(e) => setValue(e.target.value)}
-    displayEmpty
-    fullWidth
-    sx={mobileSelectSX}
+      value={value}
+      onChange={(e) => setValue(e.target.value)}
+      displayEmpty
+      fullWidth
+      sx={mobileSelectSX}
     >
       <MenuItem value="">{placeholder}</MenuItem>
       {options.map((option) =>
-      typeof option === "string" ? (
-        <MenuItem key={option} value={option}>
-          {option}
-        </MenuItem>
-      ):( 
-        <MenuItem key={option.value} value={option.value}>
-          {option.label}
-        </MenuItem>
-      )
+        typeof option === "string" ? (
+          <MenuItem key={option} value={option}>
+            {option}
+          </MenuItem>
+        ) : (
+          <MenuItem key={option.value} value={option.value}>
+            {option.label}
+          </MenuItem>
+        )
       )}
     </Select>
   );
@@ -121,103 +122,48 @@ export default function FilteringSection() {
         {isMobile ? (
           // Dropdown mobile
           <>
-          {renderSelect(genre, setGenre, "Välj genre(Ingen vald)", genres)}
-          {renderSelect(decade, setDecade, "Välj årtionde", decades)}
-           {renderSelect(filmType, setFilmType, "Välj Svartvit/Färgfilm", filmTypes)}
-         
+            {renderSelect(genre, setGenre, "Välj genre(Ingen vald)", genres)}
+            {renderSelect(decade, setDecade, "Välj årtionde", decades)}
+            {renderSelect(
+              filmType,
+              setFilmType,
+              "Välj Svartvit/Färgfilm",
+              filmTypes
+            )}
           </>
         ) : (
-          //Desktop
-          <>
-            <Box
-              sx={{
-                display: "flex",
-                gap: 1,
-                flexWrap: "wrap",
-                justifyContent: "center",
-              }}
-            >
-              {genres.map((g) => (
-                <Button
-                  key={g}
-                  variant="contained"
-                  onClick={() => {
-                    setGenre((prev) => (prev === g ? "" : g));
-                  }}
-                  sx={{
-                    backgroundColor: genre === g ? "#c43c3a" : "inherit",
-                    "&:hover": { backgroundColor: "#c43c3a" },
-                  }}
-                >
-                  {g}
-                </Button>
-              ))}
-            </Box>
+          //Imported from FilterButtons file
 
-            <Box
-              sx={{
-                display: "flex",
-                gap: 1,
-                flexWrap: "wrap",
-                justifyContent: "center",
-              }}
-            >
-              {decades.map(({ label, value }) => (
-                <Button
-                  key={value}
-                  variant="contained"
-                  onClick={() => {
-                    setDecade((prev) => (prev === value ? "" : value));
-                  }}
-                  sx={{
-                    backgroundColor: decade === value ? "#c43c3a" : "inherit",
-                    "&:hover": { backgroundColor: "#c43c3a" },
-                  }}
-                >
-                  {label}
-                </Button>
-              ))}
-            </Box>
-            <Box sx={{ display: "flex", gap: 1, justifyContent: "center" }}>
-              {filmTypes.map((type) => (
-                <Button
-                  key={type}
-                  variant="contained"
-                  onClick={() => {
-                    setFilmType((prev) => (prev === type ? "" : type));
-                  }}
-                  sx={{
-                    backgroundColor: filmType === type ? "#c43c3a" : "inherit",
-                    "&:hover": { backgroundColor: "#c43c3a" },
-                  }}
-                >
-                  {type}
-                </Button>
-              ))}
-            </Box>
-               </>
+          <FilterButtons
+            isMobile={isMobile}
+            genre={genre}
+            setGenre={setGenre}
+            decade={decade}
+            setDecade={setDecade}
+            filmType={filmType}
+            setFilmType={setFilmType}
+          />
         )}
-            <Button
-              sx={{ color: "#f1ddc5" }}
-              onClick={applyFilters}
-              variant="contained"
-            >
-              Filtrera
-            </Button>
-            <Button 
-            sx={{ color: "#f1ddc5"}}
-            onClick={() => { 
-              setGenre("");
-              setDecade("");
-              setFilmType("");
-              setSearch("");
-              router.push("/movies");
-            }}
-            variant="contained"
-            >
-              Visa alla filmer
-            </Button>
-       
+        <Button
+          sx={{ color: "#f1ddc5" }}
+          onClick={applyFilters}
+          variant="contained"
+        >
+          Filtrera
+        </Button>
+        <Button
+          sx={{ color: "#f1ddc5" }}
+          onClick={() => {
+            setGenre("");
+            setDecade("");
+            setFilmType("");
+            setSearch("");
+            router.push("/movies");
+          }}
+          variant="contained"
+        >
+          Visa alla filmer
+        </Button>
       </Box>
     </ThemeProvider>
   );
