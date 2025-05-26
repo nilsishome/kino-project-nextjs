699fbe6b1259192c33d3eb8fb41a8b51818af6ae
nilsishome <nilsnyberg24@gmail.com>
2025-05-26 12:11:31 +0200

    Add styling for current screenings on home page

--- components/index/currentScreenings.tsx
+++ components/index/currentScreenings.tsx
@@ -1,9 +1,11 @@
-import { Typography } from "@mui/material";
+import { Box, Divider, List, ListItem, Typography } from "@mui/material";
+import Image from "next/image";
 import React from "react";
 
 interface MovieTemplate {
   title: string;
   coverImage: string;
+  date?: string;
   time: string;
 }
 
@@ -13,20 +15,92 @@ const CurrentScreenings: React.FC = async () => {
   if (!response.ok) throw new Error("Failed to retrieve data!");
 
   const payload = await response.json();
-  const movieData = payload.data;
+  const movieData: MovieTemplate[] = payload.data;
 
   const dates: string[] = [];
   const movieScreenings: MovieTemplate[][] = [];
 
+  sortScreenings(movieData, dates, movieScreenings);
+
+  return (
+    <>
+      {dates.map((day: string, dateIndex: number) => (
+        <Box key={dateIndex}>
+          <Typography
+            variant="h2"
+            sx={{
+              margin: "1rem 1rem",
+              fontSize: { xs: "1.5rem", sm: "2rem" },
+            }}
+          >
+            Filmer som visas {day}
+          </Typography>
+          <Divider
+            variant="middle"
+            sx={{ bgcolor: "#f1ddc5", marginBottom: 2 }}
+          />
+          <List sx={{ display: "flex", overflow: "auto" }}>
+            {movieScreenings[dateIndex].map(
+              (movie: MovieTemplate, listIndex: number) => (
+                <ListItem
+                  key={listIndex}
+                  sx={{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    color: "#f1ddc5",
+                    width: "fit-content",
+                  }}
+                >
+                  <Image
+                    alt={`Filmposter av ${movie.title}`}
+                    src={movie.coverImage}
+                    width={150}
+                    height={220}
+                  />
+                  <Typography
+                    variant="body1"
+                    sx={{
+                      textAlign: "center",
+                      marginTop: 2,
+                      fontWeight: 600,
+                      width: 150,
+                    }}
+                  >
+                    {movie.title}
+                  </Typography>
+                  <Typography
+                    variant="body1"
+                    sx={{ textAlign: "center", marginTop: 1, marginBottom: 4 }}
+                  >
+                    {movie.time}:00
+                  </Typography>
+                </ListItem>
+              )
+            )}
+          </List>
+        </Box>
+      ))}
+    </>
+  );
+};
+
+// Algorithm for sorting movies and dates into 2 arrays
+function sortScreenings(
+  movieData: MovieTemplate[],
+  dates: string[],
+  movieScreenings: MovieTemplate[][]
+) {
   const usedDates: number[] = [];
 
   let movieIndex: number = -1;
 
   for (let i = 0; i < movieData.length; i++) {
-    const date = new Date(movieData[i].date);
+    const date = new Date(movieData[i].date!);
     const monthDay = date.getDate();
 
     if (!usedDates.includes(monthDay)) {
+      // Initialize new date
       const weekDay = date.toLocaleString("sv-SV", { weekday: "long" });
       const month = date.getMonth() + 1;
 
@@ -47,16 +121,6 @@ const CurrentScreenings: React.FC = async () => {
 
     movieScreenings[movieIndex].push(movie);
   }
-
-  return (
-    <>
-      {dates.map((day: string, index: number) => (
-        <Typography key={index} variant="h2">
-          Filmer som visas {day}
-        </Typography>
-      ))}
-    </>
-  );
-};
+}
 
 export default CurrentScreenings;
