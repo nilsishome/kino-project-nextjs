f64a36be8f109b617bb0888b6f1c1160522d6e1a
nilsishome <nilsnyberg24@gmail.com>
2025-06-06 17:07:08 +0200

    Change movies page to client component

--- src/app/movies/page.tsx
+++ src/app/movies/page.tsx
@@ -1,24 +1,41 @@
+"use client";
 import { Movie } from "@/types";
 import MovieFilter from "../../../components/movies/MovieFilter";
 
 import { Box } from "@mui/material";
 import * as styles from "../styles/moviesPage.style";
+import { Suspense, useEffect, useState } from "react";
 
-export default async function Page() {
-  const response = await fetch(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/api/movies`
-  );
+export default function Page() {
+  const [movies, setMovies] = useState<Movie[]>([]);
 
-  if (!response.ok) {
-    throw new Error("Failed to retrieve data!");
-  }
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          `${process.env.NEXT_PUBLIC_BASE_URL}/api/movies`
+        );
 
-  const payload = await response.json();
-  const movies: Movie[] = payload.data;
+        if (!response.ok) {
+          throw new Error("Failed to retrieve data!");
+        }
+
+        const payload = await response.json();
+
+        setMovies(payload.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
-    <Box sx={styles.container}>
-      <MovieFilter movies={movies} />
-    </Box>
+    <Suspense>
+      <Box sx={styles.container}>
+        <MovieFilter movies={movies} />
+      </Box>
+    </Suspense>
   );
 }
