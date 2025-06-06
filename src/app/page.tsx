ce304623ac7ad8759b7b56bbe83a8b818c13ab88
Jörgen Lindström <jorgen.lindstrom@learniastudent.se>
2025-06-06 17:26:49 +0200

    adding Themeprovider in app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+import { ThemeProvider } from "@mui/material/styles";
+// Update the import path below if your theme file is located elsewhere
+import theme from "../theme";
 import CarouselComponent from "../../components/index/carousel";
 import FilteringSection from "../../components/index/FilteringSection";
 import GiftSection from "../../components/index/giftSection";
@@ -8,10 +11,12 @@ import CurrentScreenings from "../../components/index/currentScreenings";
 export default function Home() {
   return (
     <>
+    <ThemeProvider theme={theme}>
       <FilteringSection />
       <CarouselComponent />
       <CurrentScreenings />
       <GiftSection />
+      </ThemeProvider>
     </>
   );
 }
